d40ba2e9683db6a50c40878fedb0eab4cb37eb73
shin-seoung-gyun <sg0220.shin@daum.net>
2021-08-02 12:48:32 +0900

    pagevo upload

---
+++ HealthDiary/src/diary/PageVO.java
@@ -0,0 +1,52 @@
+package diary;
+
+public class PageVO {
+	private int page;
+	private int startPage;
+	private int endPage;
+	private boolean prev,next;
+	private int amount = 10;
+	
+	public int getAmount() {
+		return amount;
+	}
+	public int getPage() {
+		return page;
+	}
+	public int getStartPage() {
+		return startPage;
+	}
+	public int getEndPage() {
+		return endPage;
+	}
+	public boolean isPrev() {
+		return prev;
+	}
+	public boolean isNext() {
+		return next;
+	}
+	
+	public PageVO(int page, TotalVO tvo) {
+		this.page = page;
+		endPage = (int)(Math.ceil(page/10.0)*10);//Math.ceil 올림 함수
+		startPage = endPage-9;
+		int realEnd = (int)Math.ceil((double)tvo.getTotal()/amount);//실제 끝페이지
+		if(realEnd<endPage) {
+			endPage = realEnd;
+		}
+		prev = startPage >1;
+		next = endPage<realEnd;
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
---
+++ HealthDiary/src/diary/TotalVO.java
@@ -0,0 +1,13 @@
+package diary;
+
+public class TotalVO {
+	private int total;
+
+	public int getTotal() {
+		return total;
+	}
+
+	public void setTotal(int total) {
+		this.total = total;
+	}
+}
