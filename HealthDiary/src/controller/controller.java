0999a8e15aff9ef982668e5e257ebc1dfb9e7673
shin-seoung-gyun <sg0220.shin@daum.net>
2021-08-15 23:13:52 +0900

    test

--- HealthDiary/src/controller/controller.java
+++ HealthDiary/src/controller/controller.java
@@ -25,6 +25,7 @@ public class controller extends HttpServlet {
 
 	}
 
+	
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		doWork(request, response);
